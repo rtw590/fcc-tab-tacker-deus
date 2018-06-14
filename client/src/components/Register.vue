7dc9224ea2086eb6b9924b7a6d6aab4a6078c311
Rob Waller <r.t.waller@gmail.com>
2018-06-14 17:12:57 -0500

    Connected backend with frontend with axios

---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input 
+      type="email" 
+      name="email" 
+      v-model="email"
+      placeholder="email" />
+    <br>
+    <input 
+      type="password" 
+      name="password" 
+      v-model="password"
+      placeholder="password" />
+    <br>
+    <button 
+      @click="register">
+      Register
+    </button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from "@/services/AuthenticationService";
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    // Button has on click to call this function from AuthenticationService
+    // register is the name of the function on the click event
+    async register() {
+      // this runction comes from /services/AuthenticationService
+      await AuthenticationService.register({
+        // the backend will receive this information
+        email: this.email,
+        password: this.password
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
