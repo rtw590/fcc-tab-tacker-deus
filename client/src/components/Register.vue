9e6db6130c7f2b12828993896a3bd54f3188e18d
Rob Waller <r.t.waller@gmail.com>
2018-06-18 11:13:50 -0500

    Added vuetify and started styling

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,26 +1,36 @@
 <template>
-  <div>
-    <h1>Register</h1>
-
-    <input 
-      type="email" 
-      name="email" 
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input 
-      type="password" 
-      name="password" 
-      v-model="password"
-      placeholder="password" />
-    <br>
-    <div v-html="error" class="error"></div>
-    <br>
-    <button 
-      @click="register">
-      Register
-    </button>
-  </div>
+<v-app>
+  <v-layout column>
+    <v-flex xs6 offset-sx3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input 
+            type="email" 
+            name="email" 
+            v-model="email"
+            placeholder="email" />
+          <br>
+          <input 
+            type="password" 
+            name="password" 
+            v-model="password"
+            placeholder="password" />
+          <br>
+          <div v-html="error" class="error"></div>
+          <br>
+          <v-btn
+          class="cyan"
+            @click="register">
+            Register
+          </v-btn>
+      </div>
+    </div>
+    </v-flex>
+  </v-layout>
+</v-app>
 </template>
 
 <script>
